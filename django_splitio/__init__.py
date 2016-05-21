3e7cbd3741936919026ef31ed666d6932de43f29
Agustin Barto <abarto@machinalis.com>
2016-05-21 13:46:11 -0300

    Remove app config. Add splitio tags.

--- django_splitio/__init__.py
+++ django_splitio/__init__.py
@@ -1,3 +1,1 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
-default_app_config = 'django_splitio.apps.SplitioConfig'
--- django_splitio/apps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from django.apps import AppConfig
-
-
-class SplitioConfig(AppConfig):
-    name = 'splitio'
-    verbose_name = "split.io Support for Django"
