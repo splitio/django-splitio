84c221596ce2a09dba6289c7edc4004ddac4b493
Agustin Barto <abarto@machinalis.com>
2016-06-02 09:37:01 -0300

    Fix redis factory

--- django_splitio/cache.py
+++ django_splitio/cache.py
@@ -524,6 +524,6 @@ def default_redis_factory():
     """
     host = settings.SPLITIO.get('REDIS_HOST', 'localhost') or 'localhost'
     port = settings.SPLITIO.get('REDIS_PORT', 6379) or 6379
-    db = settings.SPLITIO.get('REDIS_PORT', 0) or 0
+    db = settings.SPLITIO.get('REDIS_DB', 0) or 0
     redis = StrictRedis(host=host, port=port, db=db)
     return redis
