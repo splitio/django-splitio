d5e6f0b7409a4055a0a09f29d648e34fa2ff538a
Sebastian Arrubia <sarrubia@gmail.com>
2017-02-17 19:38:37 -0300

    Version 2.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from os import path
 from setuptools import setup
 
-install_requires = ['splitio_client>=2.2']
+#install_requires = ['splitio_client>=2.2']
 
 with open(path.join(path.abspath(path.dirname(__file__)), 'django_splitio', 'version.py')) as f:
     exec (f.read())
@@ -15,7 +15,7 @@ setup(name='django_splitio',
       author_email='pato@split.io, sebastian@split.io',
       url='https://github.com/splitio/django-splitio',
       license='Apache License 2.0',
-      install_requires=install_requires,
+#      install_requires=install_requires,
       extras_require={
           'redis': ['splitio_client[redis]>=2.2'],
           'uwsgi': ['splitio_client[uwsgi]>=2.2']
