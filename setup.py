5b89dc60bcf91a85424ef941719a21ed467e579d
Sebastian Arrubia <sarrubia@gmail.com>
2017-02-17 16:41:12 -0300

    Adding uwsgi support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from os import path
 from setuptools import setup
 
-install_requires = ['splitio_client[redis]>=2.1']
+install_requires = ['splitio_client>=2.2']
 
 with open(path.join(path.abspath(path.dirname(__file__)), 'django_splitio', 'version.py')) as f:
     exec (f.read())
@@ -16,6 +16,10 @@ setup(name='django_splitio',
       url='https://github.com/splitio/django-splitio',
       license='Apache License 2.0',
       install_requires=install_requires,
+      extras_require={
+          'redis': ['splitio_client[redis]>=2.2'],
+          'uwsgi': ['splitio_client[uwsgi]>=2.2']
+      },
       setup_requires=['flake8', 'coverage'],
       classifiers=[
           'Development Status :: 2 - Pre-Alpha',
